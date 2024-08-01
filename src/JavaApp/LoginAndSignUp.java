4ac92824df51ac0fff72b8d0038e956e0b5f028e
Arti Manandhar <artimanandhar@gmail.com>
2024-08-01 20:39:17 +0545

    Final Frontend

---
+++ src/JavaApp/LoginAndSignUp.java
@@ -0,0 +1,19 @@
+
+package JavaApp;
+import JavaApp.View.Login;
+import com.formdev.flatlaf.extras.FlatSVGIcon;
+
+
+public class LoginAndSignUp {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+         Login LoginFrame=new Login();
+         LoginFrame.setVisible(true);
+         LoginFrame.pack();
+         LoginFrame.setLocationRelativeTo(null);
+         
+    }
+    
+}
